71fb7ac4001726903069e78c5b90b0db40b80a43
Ethan Evans <ethanalexevans@gmail.com>
2022-07-24 00:57:45 -0700

    added display keyword

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,136 @@
 mod lexer;
 mod parser;
 
+fn run_program(program: &Vec<parser::Expr>) {
+    let mut i = 0;
+    while i < program.len() {
+        match &program[i] {
+            parser::Expr::Display(expr) => {
+                println!("{}", expr.eval());
+            }
+            _ => {}
+        };
+        i += 1;
+    }
+}
+
+
 fn main() {
-    let lex_result = lexer::lex("a <- something");
-    println!("{:?}", parser::parse(lex_result));
+    let lex_result = lexer::lex("DISPLAY 1 + 1");
+    let parse_result = parser::parse(lex_result);
+    run_program(&parse_result);
+}
 
-    let lex_result = lexer::lex("a <- 1");
-    println!("{:?}", parser::parse(lex_result));
+#[cfg(test)]
+mod tests {
+    use crate::*;
 
-    let lex_result = lexer::lex("a <- \"string value\"");
-    println!("{:?}", parser::parse(lex_result));
+    #[test]
+    fn test_assign_identifier() {
+        let lex_result = lexer::lex("a <- something");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Identifier("something".to_string()))
+            )
+        );
+    }
+
+    #[test]
+    fn test_assign_int_literal() {
+        let lex_result = lexer::lex("a <- 1");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Literal(parser::Literal::Int(1)))
+            )
+        );
+    }
+
+    #[test]
+    fn test_assign_string_literal() {
+        let lex_result = lexer::lex("a <- \"something\"");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Literal(parser::Literal::String("something".to_string())))
+            )
+        );
+    }
+
+    #[test]
+    fn test_assign_bool_literal() {
+        let lex_result = lexer::lex("a <- true");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Literal(parser::Literal::Bool(true)))
+            )
+        );
+    }
+
+    #[test]
+    fn test_parens() {
+        let lex_result = lexer::lex("a <- (1 + 2)");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Group(
+                    Box::new(parser::Expr::BinaryOp(
+                        parser::Operation::Plus,
+                        Box::new(parser::Expr::Literal(parser::Literal::Int(1))),
+                        Box::new(parser::Expr::Literal(parser::Literal::Int(2)))
+                    ))
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test_index() {
+        let lex_result = lexer::lex("a <- [1, 1]");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::BinaryOp(
+                parser::Operation::Assign,
+                Box::new(parser::Expr::Identifier("a".to_string())),
+                Box::new(parser::Expr::Literal(parser::Literal::List(vec![
+                    parser::Expr::Literal(parser::Literal::Int(1)),
+                    parser::Expr::Literal(parser::Literal::Int(1))
+                ])))
+            )
+        );
+    }
+
+    #[test]
+    fn test_display() {
+        let lex_result = lexer::lex("DISPLAY 1 + 1");
+        let parse_result = parser::parse(lex_result);
+        assert_eq!(
+            parse_result[0],
+            parser::Expr::Display(
+                Box::new(parser::Expr::BinaryOp(
+                    parser::Operation::Plus,
+                    Box::new(parser::Expr::Literal(parser::Literal::Int(1))),
+                    Box::new(parser::Expr::Literal(parser::Literal::Int(1)))
+                ))
+            )
+        );
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use crate::lexer::Symbols;
 
 #[derive(Debug, PartialEq)]
-enum Operation {
+pub enum Operation {
     Assign,
     Plus,
     Minus,
@@ -11,10 +11,21 @@ enum Operation {
 }
 
 #[derive(Debug, PartialEq)]
-enum Literal {
+pub enum Literal {
     Int(i32),
     Float(f32),
     String(String),
+    Bool(bool),
+    List(Vec<Expr>)
+}
+
+impl Literal {
+    pub fn eval(&self) -> i32 {
+        match self {
+            Literal::Int(i) => *i,
+            _ => 0
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -24,7 +35,38 @@ pub enum Expr {
     BinaryOp(Operation, Box<Expr>, Box<Expr>),
     UnaryOp(String, Box<Expr>),
     Call(String),
-    Grouping(Box<Expr>),
+    Group(Box<Expr>),
+    Index(Box<Expr>),
+    Block(Vec<Expr>),
+
+    Display(Box<Expr>),
+    Conditional(Box<Expr>, Box<Expr>, Box<Expr>),
+    RepeatNTimes(Box<Expr>, Box<Expr>),
+    RepeatUntil(Box<Expr>, Box<Expr>),
+}
+
+impl Expr {
+    pub fn eval(&self) -> i32 {
+        match self {
+            Expr::Literal(literal) => literal.eval(),
+            //Expr::Identifier(identifier) => identifier,
+            Expr::BinaryOp(operation, left, right) => {
+                let left_eval = left.eval();
+                let right_eval = right.eval();
+                match operation {
+                    Operation::Plus => left_eval + right_eval,
+                    Operation::Minus => left_eval - right_eval,
+                    Operation::Star => left_eval * right_eval,
+                    Operation::Slash => left_eval / right_eval,
+                    _ => unimplemented!()
+                    //Operation::Compare(symbol) => format!("{} {} {}", left_eval, symbol.to_string(), right_eval)
+                }
+            }
+            
+            Expr::Display(expr) => expr.eval(),
+            _ => unimplemented!()
+        }
+    }
 }
 
 pub fn parse(tokens: Vec<Symbols>) -> Vec<Expr> {
@@ -42,7 +84,10 @@ pub fn parse(tokens: Vec<Symbols>) -> Vec<Expr> {
 
 fn parse_step(tokens: &Vec<Symbols>, index: usize) -> Expr {
     match &tokens[index] {
-        Symbols::Identifier(ident) => Expr::Identifier(ident.to_string()),
+        Symbols::Identifier(ident) => match ident.as_str() {
+            "true" | "false" => Expr::Literal(Literal::Bool(ident.parse::<bool>().unwrap())),
+            _ => Expr::Identifier(ident.to_string())
+        },
         Symbols::Number(number) => Expr::Literal(Literal::Int(number.parse().unwrap())),
         Symbols::String(string) => Expr::Literal(Literal::String(string.to_string())),
         _ => match descend(tokens, index){
@@ -77,21 +122,150 @@ fn parse_binary_op(tokens: &Vec<Symbols>, index: usize) -> Option<Expr> {
     ))
 }
 
-fn descend(tokens: &Vec<Symbols>, index: usize) -> Option<Expr> {
-    match tokens[index] {
-        Symbols::Assign
-        | Symbols::Plus
-        | Symbols::Minus
-        | Symbols::Star
-        | Symbols::Slash
-        | Symbols::Greater
-        | Symbols::GreaterEqual
-        | Symbols::Less
-        | Symbols::LessEqual
-        | Symbols::NotEqual
-        | Symbols::Equal => parse_binary_op(tokens, index),
-        Symbols::LParen => todo!("parse_grouping"),
+fn parse_list(tokens: &Vec<Symbols>, index: usize) -> Option<Vec<Expr>> {
+    let mut result: Vec<Expr> = Vec::new();
+    let mut i = index + 1;
+    while i < tokens.len() {
+        match &tokens[i] {
+            Symbols::Comma => {
+                result.push(parse_step(tokens, i - 1));
+                println!("comma: {:?}", result);
+                i += 1;
+            }
+            _ => {
+                result.push(parse_step(tokens, i));
+                println!("other: {:?}", result);
+                i += 1;
+            }
+        }
+    }
+    Some(result)
+}
 
+fn descend(tokens: &Vec<Symbols>, index: usize) -> Option<Expr> {
+    match tokens.get(index) {
+        Some(Symbols::Assign)
+        | Some(Symbols::Plus)
+        | Some(Symbols::Minus)
+        | Some(Symbols::Star)
+        | Some(Symbols::Slash)
+        | Some(Symbols::Greater)
+        | Some(Symbols::GreaterEqual)
+        | Some(Symbols::Less)
+        | Some(Symbols::LessEqual)
+        | Some(Symbols::NotEqual)
+        | Some(Symbols::Equal) => parse_binary_op(tokens, index),
+        Some(Symbols::LParen) => {
+            let mut paren_count = 1;
+            let mut i = index + 1;
+            let mut inner: Vec<Symbols> = Vec::new();
+            while paren_count > 0 {
+                match tokens[i] {
+                    Symbols::LParen => paren_count += 1,
+                    Symbols::RParen => paren_count -= 1,
+                    _ => {}
+                };
+                if paren_count > 0 {
+                    inner.push(tokens[i].clone());
+                    i += 1;
+                }                
+            }
+
+            let mut i2 = index + 1;
+            let mut result: Option<Expr> = None;
+            while match descend(&tokens, i2 + 1) {
+                Some(expr) => {result = Some(expr); false},
+                None => {true}
+            } { i2 += 1; }
+            match result {
+                Some(expr) => Some(Expr::Group(Box::new(expr))),
+                None => panic!("Invalid grouping at position {}", index)
+            }
+        },
+        Some(Symbols::LBrace) => {
+            let mut paren_count = 1;
+            let mut i = index + 1;
+            let mut inner: Vec<Symbols> = Vec::new();
+            while paren_count > 0 {
+                match tokens[i] {
+                    Symbols::LBrace => paren_count += 1,
+                    Symbols::RBrace => paren_count -= 1,
+                    _ => {}
+                };
+                if paren_count > 0 {
+                    inner.push(tokens[i].clone());
+                    i += 1;
+                }
+            }
+
+            Some(Expr::Block(parse(inner)))
+        },
+        Some(Symbols::LBracket) => {
+            let mut paren_count = 1;
+            let mut i = index + 1;
+            let mut inner: Vec<Symbols> = Vec::new();
+            while paren_count > 0 {
+                match tokens[i] {
+                    Symbols::LBracket => paren_count += 1,
+                    Symbols::RBracket => paren_count -= 1,
+                    _ => {}
+                };
+                if paren_count > 0 {
+                    inner.push(tokens[i].clone());
+                    i += 1;
+                }
+            }
+
+            let contents = match parse_list(&inner, 0) {
+                Some(list) => list,
+                None => panic!("Invalid list at position {}", index)
+            };
+            Some(Expr::Literal(Literal::List(contents)))
+        },
+        Some(Symbols::Identifier(ident)) => {
+            match ident.as_str() {
+                "DISPLAY" => {
+                    let mut i2 = index + 1;
+                    let mut operand: Option<Expr> = None;
+                    while match descend(&tokens, i2 + 1) {
+                        Some(expr) => {operand = Some(expr); false},
+                        None => {true}
+                    } { i2 += 1; }
+                    match operand {
+                        Some(expr) => Some(Expr::Display(Box::new(expr))),
+                        None => panic!("Invalid grouping at position {}", index)
+                    }
+                },
+                "RANDOM"
+
+                | "NOT"
+                | "AND"
+                | "OR"
+
+                | "IF"
+                | "ELSE"
+                | "REPEAT"
+                | "UNTIL"
+                | "TIMES"
+
+                | "INSERT"
+                | "APPEND"
+                | "REMOVE"
+                | "LENGTH"
+
+                | "FOR"
+                | "EACH"
+                | "IN"
+
+                | "PROCEDURE"
+                | "RETURN" => {
+                    todo!()
+                }
+                _ => {
+                    None
+                }
+            }
+        }
         _ => None
     }
 }
